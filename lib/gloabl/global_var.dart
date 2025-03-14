b19653fcc26734723d48141892f8da9c64995856
feryalbassam <feryalbassam200004@gmail.com>
2025-03-14 16:22:50 +0400

    Updated feature XYZ

--- lib/gloabl/global_var.dart
+++ lib/gloabl/global_var.dart
@@ -1,7 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = '';
-String googleMapKey = 'AIzaSyDqqo8ReK016pMnLrF3DdICvbE53lHw8jc';
+String googleMapKey = 'AIzaSyBAhgkDwLYH0Y7s9EMKjk3VZ1k0yu6GOZ8';
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
   zoom: 14.4746,
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gas_on_go/pages/home_page.dart';
 import 'package:gas_on_go/splash_screen.dart';
 
 Future<void> main() async {
